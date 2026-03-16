745b67e6280ba01b7c8e75783bc19c45a5feff30
DEVX STUDIO <devudevansh22@gmail.com>
2026-03-16 10:12:34 +0530

    Add event listeners and font change functions

---
+++ app.js
@@ -0,0 +1,32 @@
+let b= document.getElementsByTagName("button");
+b[0].addEventListener('click' , function (){
+document.body.style.backgroundColor = "#333";
+})
+
+b[1].addEventListener('click' , function (){
+document.body.style.backgroundColor = "#ffffff";
+})
+function changeFont1(){
+    document.body.style.fontFamily = "Arial, sans-serif";
+}
+function changeFont2(){
+    document.body.style.fontFamily = "Georgia, serif";
+}
+function changeFont3(){
+    document.body.style.fontFamily = "Courier New, monospace";
+}
+function changeFont4(){
+    document.body.style.fontFamily = "Cursive";
+}
+function changeFont5(){
+    document.body.style.fontFamily = "Fantasy";
+}
+function changeFont6(){
+    document.body.style.fontFamily = "Times New Roman, serif";
+}
+function changeFont7(){
+    document.body.style.fontFamily = "Verdana, sans-serif";
+}
+function changeFont8(){
+    document.body.style.fontFamily = "Trebuchet MS, sans-serif";
+}
